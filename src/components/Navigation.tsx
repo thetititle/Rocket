d0e8a32d05d103082fa999561df32553ec08e6d0
thetititle@gmail.com <business.thetititle@gmail.com>
2024-04-15 22:20:15 +0900

    style : markup Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,8 +1,89 @@
+import { NavLink, Link } from 'react-router-dom';
 import styled from 'styled-components';
+import logo from '../logo.svg';
+import homeIcon from '../asset/img/rocket.png';
+import exporteIcon from '../asset/img/expore.png';
+import notificatuonIcon from '../asset/img/notification.png';
+import bookmarkIcon from '../asset/img/bookmark.png';
+
 export const Wrapper = styled.div`
   flex: 1;
   padding: 0 20px;
 `;
+export const Logo = styled.img`
+  height: 54px;
+  object-fit: cover;
+  padding: 5px;
+`;
+export const Icon = styled.img`
+  height: 20px;
+  object-fit: cover;
+`;
+export const Ul = styled.ul`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 10px;
+`;
+export const Li = styled.li`
+  line-height: 54px;
+  opacity: 0.25;
+  a {
+    align-items: center;
+    gap: 10px;
+    display: flex;
+    color: inherit;
+  }
+  &.active {
+    opacity: 1;
+  }
+`;
+export const NavWrap = styled.div`
+  flex: 1;
+`;
+export const FireBtn = styled.button`
+  color: var(--fontDefault);
+  background-color: var(--highLight);
+  &:hover {
+    color: var(--fontDefault);
+    background-color: var(--highLight);
+  }
+`;
+
 export default function Navigation() {
-  return <Wrapper>NAV</Wrapper>;
+  return (
+    <Wrapper>
+      <Link to="/home">
+        <Logo src={logo} />
+      </Link>
+      <Ul>
+        <Li>
+          <NavLink to="/home">
+            <Icon src={homeIcon} />
+            <h2>Home</h2>
+          </NavLink>
+        </Li>
+        <Li>
+          <NavLink to="#">
+            <Icon src={exporteIcon} />
+            <h2>Expore</h2>
+          </NavLink>
+        </Li>
+        <Li>
+          <NavLink to="#">
+            <Icon src={notificatuonIcon} />
+            <h2>Notification</h2>
+          </NavLink>
+        </Li>
+        <Li>
+          <NavLink to="#">
+            <Icon src={bookmarkIcon} />
+            <h2>Bookmark</h2>
+          </NavLink>
+        </Li>
+      </Ul>
+      <FireBtn>
+        <h2>FIRE</h2>
+      </FireBtn>
+    </Wrapper>
+  );
 }
